0291bee926695d0fe551b38f9b8e06ddf1928dd4
Sherief Farid <sherief@farid.cloud>
2019-03-02 19:39:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require 'pry'
 def reverse_each_word(string)
   string_array = string.split
-   string_array.collect do  |word|
      rev_string = ""
+   string_array.collect do  |word|
      rev_word = word.reverse
      puts rev_string + rev_word
 binding.pry

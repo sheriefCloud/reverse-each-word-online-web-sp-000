f250faadf6c3249e812488ecf2dcd51d684ab214
Sherief Farid <sherief@farid.cloud>
2019-03-02 20:18:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,10 @@ def reverse_each_word(string)
   rev_string = ""
   string_array.collect do  |word|
     rev_word = word.reverse
-    rev_string = "#{rev_string}#{rev_word} "
+    #rev_string = "#{rev_string}#{rev_word} "
     end
-    puts rev_string
-binding.pry
-   return rev_string
+    puts string_array
+    #puts rev_string
+#binding.pry
+   #Sreturn rev_string
 end

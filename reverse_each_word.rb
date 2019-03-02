492cae2cd3835ff4e4f4be86c12bf6dbbf515e60
Sherief Farid <sherief@farid.cloud>
2019-03-02 19:40:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,7 @@ def reverse_each_word(string)
    string_array.collect do  |word|
      rev_word = word.reverse
      rev_string = rev_string + rev_word
+     puts rev_string
 binding.pry
    end
    return rev_string

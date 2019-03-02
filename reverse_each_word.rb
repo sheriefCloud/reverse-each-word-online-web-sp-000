361816fac53d72d6bd697148c689097ed7d554e7
Sherief Farid <sherief@farid.cloud>
2019-03-02 20:14:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,12 @@
 require 'pry'
 def reverse_each_word(string)
   string_array = string.split
-     rev_string = ""
-   string_array.collect do  |word|
-     rev_word = word.reverse
-     if string_array.first
-     rev_string = "#{rev_word} "
-     #solve for the first word or fo rhte last word
-    else
-     rev_string = "#{rev_string} #{rev_word}"
+  rev_string = ""
+  string_array.collect do  |word|
+    rev_word = word.reverse
+    rev_string = "#{rev_string} #{rev_word}"
     end
-     puts rev_string
+    puts rev_string
 binding.pry
-   end
    return rev_string
 end
